240b92f348f1bd1c7328be676721f8efbc383fd5
DorianHannah <dorian.hannah@polymtl.ca>
2022-11-23 13:28:23 -0500

    link la page planrepas

--- Application_Vierge_Nouvelle_Version_Nodemon/client/src/app/modules/app-routing.module.ts
+++ Application_Vierge_Nouvelle_Version_Nodemon/client/src/app/modules/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
 import { AppComponent } from "../app.component";
+import { PlanRepasComponent } from "../pages/plan-repas/plan-repas.component";
 
 const routes: Routes = [
-  { path: "app", component: AppComponent }
+  { path: "app", component: AppComponent },
+  { path: "app/planrepas", component: PlanRepasComponent }
 ];
 
 @NgModule({
